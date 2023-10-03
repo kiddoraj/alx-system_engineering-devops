e266fcfc79a5e935451db91db54606be698e8c3c
rajy <kiddo.rajy@gmail.com>
2023-10-03 05:33:26 -0700

    Debugg

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,9 @@
-# Define a class for configuring Nginx with a custom HTTP header
-class custom_http_response_header {
+# Automation: creates a custom HTTP header response with Puppet.
+exec { 'command':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
 
-  # Install Nginx package
-  package { 'nginx':
-    ensure => installed,
-  }
-
-  # Create a custom fact to get the hostname of the server
-  # This fact will be used to set the value of the custom header
-  facterng::fact { 'server_hostname':
-    value => $::hostname,
-  }
-
-  # Configure Nginx with the custom HTTP response header
-  file { '/etc/nginx/conf.d/custom_header.conf':
-    ensure  => file,
-    content => "add_header X-Served-By $::server_hostname;",
-    require => Package['nginx'],
-    notify  => Service['nginx'],
-  }
-
-  # Ensure Nginx service is running and enabled
-  service { 'nginx':
-    ensure  => running,
-    enable  => true,
-    require => Package['nginx'],
-  }
 }
-
-# Include the custom_http_response_header class
-include custom_http_response_header

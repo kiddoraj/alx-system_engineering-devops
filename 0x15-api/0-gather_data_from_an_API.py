b720b606e87872200a200f75aabc293190a219f8
rajy <kiddo.rajy@gmail.com>
2023-10-23 13:49:34 -0700

    PEP 8 pass

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,52 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""
+Module to fetch and display employee TODO list progress using a REST API.
+"""
+
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+def fetch_todo_list(employee_id):
+    """
+    Fetch and display employee TODO list progress.
+
+    Args:
+        employee_id (int): The employee ID to fetch information for.
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/todos?userId={employee_id}"
+
+    try:
+        user_response = requests.get(user_url)
+        todo_response = requests.get(todo_url)
+
+        if (user_response.status_code != 200 or
+                todo_response.status_code != 200):
+            print("Error: Unable to fetch data from the API.")
+            return
+
+        user_data = user_response.json()
+        todo_data = todo_response.json()
+
+        employee_name = user_data["name"]
+        total_tasks = len(todo_data)
+        done_tasks = [task for task in todo_data if task["completed"]]
+
+        print(f"Employee {employee_name} is done with "
+              f"tasks ({len(done_tasks)}/{total_tasks}):")
+        for task in done_tasks:
+            print(f"\t{task['title']}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request Error: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <EMPLOYEE_ID>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    fetch_todo_list(employee_id)
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,68 @@
 #!/usr/bin/python3
-"""Exports to-do list information for a given employee ID to JSON format."""
-import json
+"""
+Module to fetch employee TODO list progress and export it to a CSV file.
+"""
+
+import csv
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+def fetch_todo_list(employee_id):
+    """
+    Fetch employee TODO list progress and export it to a CSV file.
+
+    Args:
+        employee_id (int): The employee ID to fetch information for.
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+    user_url = f"{base_url}/users/{employee_id}"
+    todo_url = f"{base_url}/todos?userId={employee_id}"
+
+    try:
+        user_response = requests.get(user_url)
+        todo_response = requests.get(todo_url)
+
+        if (user_response.status_code != 200 or
+                todo_response.status_code != 200):
+            print("Error: Unable to fetch data from the API.")
+            return
+
+        user_data = user_response.json()
+        todo_data = todo_response.json()
+
+        employee_name = user_data["username"]
+
+        # Prepare CSV file name based on USER_ID
+        csv_filename = f"{employee_id}.csv"
+
+        with open(csv_filename, mode='w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+
+            # Write CSV header
+            csv_writer.writerow(
+                ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+            for task in todo_data:
+                task_completed = "Yes" if task["completed"] else "No"
+                csv_writer.writerow(
+                    [user_data["id"],
+                     employee_name,
+                     task_completed,
+                     task["title"]])
+
+        print(
+            f"Employee {employee_name}'s tasks have"
+            "been exported to {csv_filename}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request Error: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <EMPLOYEE_ID>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    fetch_todo_list(employee_id)

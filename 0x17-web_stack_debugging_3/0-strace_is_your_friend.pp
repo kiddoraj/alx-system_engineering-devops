aa26bbe489efc0169bebf87f4d532187ea0e6d14
rajy <kiddo.rajy@gmail.com>
2023-11-08 11:08:27 -0800

    Tasks Completed

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,8 +2,7 @@
 exec { 'fix-wordpress':
   # The title uniquely identifies this resource
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  
-  # Specify the command to be executed
+    # Specify the command to be executed
   # This 'sed' command replaces 'phpp' with 'php' globally in the specified file
   # '-i' flag indicates an in-place edit
   path    => '/usr/local/bin/:/bin/',
